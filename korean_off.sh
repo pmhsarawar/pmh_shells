a2a2891619d0614944d1eff8d7b8f1d930dc9f57
pmhsarawar <61244650+pmhsarawar@users.noreply.github.com>
2020-02-20 20:32:34 +0530

    check
    cjeck

--- korean_off.sh
+++ korean_off.sh
@@ -1,1 +1,31 @@
-echo "This is korean off shell"
+am force-stop com.tencent.ig > /dev/null 2>&1
+am force-stop com.vng.pubgmobile > /dev/null 2>&1
+am force-stop com.pubg.krmobile > /dev/null 2>&1
+sleep 1 > /dev/null 2>&1
+rm -r /sdcard/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/game_patch_0.16.5.11551.pak
+sleep 1 > /dev/null 2>&1
+chmod 771 /data/data/com.pubg.krmobile/databases > /dev/null 2>&1
+sleep 1
+cp /storage/emulated/0/Android/data/a.a.a/files/korea/ogi/puffer_res.eifs /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/puffer_res.eifs
+cp /storage/emulated/0/Android/data/a.a.a/files/korea/ogi/apollo_reslist.flist /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/apollo_reslist.flist
+cp /storage/emulated/0/Android/data/a.a.a/files/korea/ogi/filelist.json /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/filelist.json
+cp /storage/emulated/0/Android/data/a.a.a/files/korea/ogi/PufferFileList.json /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/PufferFileList.json
+sleep 1 > /dev/null 2>&1
+rm -rf /storage/emulated/.backups/* > /dev/null 2>&1
+rm -rf /storage/emulated/0/MidasOversea/* &> /dev/null 2>&1
+rm -rf /storage/emulated/0/Tencent/* &> /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/cache > /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/vmpcloudconfig.json > /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/login-identifier.txt > /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/ca-bundle.pem > /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/cacheFile.txt > /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/tbslog > /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/config.igm > /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/GameErrorNoRecords > /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/ImageDownload > /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Pandora > /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/Engine > /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/StatEventReportedFlag > /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/RoleInfo > /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/UpdateInfo > /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs > /dev/null 2>&1
