45fbaeee46513b19345e81fa8ec8867a96a98f59
pmhsarawar <61244650+pmhsarawar@users.noreply.github.com>
2020-02-21 23:16:25 +0530

    fdsaf
    asdfs

--- korean_off.sh
+++ korean_off.sh
@@ -1,3 +1,4 @@
+sleep 1
 am force-stop com.tencent.ig > /dev/null 2>&1
 am force-stop com.vng.pubgmobile > /dev/null 2>&1
 am force-stop com.pubg.krmobile > /dev/null 2>&1
@@ -6,10 +7,10 @@ rm -r /sdcard/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/Sh
 sleep 1 > /dev/null 2>&1
 chmod 771 /data/data/com.pubg.krmobile/databases > /dev/null 2>&1
 sleep 1
-cp /storage/emulated/0/Android/data/a.a.a/files/korea/ogi/puffer_res.eifs /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/puffer_res.eifs
-cp /storage/emulated/0/Android/data/a.a.a/files/korea/ogi/apollo_reslist.flist /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/apollo_reslist.flist
-cp /storage/emulated/0/Android/data/a.a.a/files/korea/ogi/filelist.json /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/filelist.json
-cp /storage/emulated/0/Android/data/a.a.a/files/korea/ogi/PufferFileList.json /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/PufferFileList.json
+cp /storage/emulated/0/Android/data/a.a.a/files/m/o/pmh.1 /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/puffer_res.eifs
+cp /storage/emulated/0/Android/data/a.a.a/files/m/o/pmh.2 /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/apollo_reslist.flist
+cp /storage/emulated/0/Android/data/a.a.a/files/m/o/pmh.3 /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/filelist.json
+cp /storage/emulated/0/Android/data/a.a.a/files/m/o/pmh.4 /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/PufferFileList.json
 sleep 1 > /dev/null 2>&1
 rm -rf /storage/emulated/.backups/* > /dev/null 2>&1
 rm -rf /storage/emulated/0/MidasOversea/* &> /dev/null 2>&1
@@ -29,3 +30,8 @@ rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTr
 rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/RoleInfo > /dev/null 2>&1
 rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/UpdateInfo > /dev/null 2>&1
 rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs > /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/GameErrorNoRecords > /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Screenshots > /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/PufferTmpDir > /dev/null 2>&1
+rm -f /data/cache/magisk.log &> /dev/null 2>&1
+rm -f /data/cache/magisk.log.bak &> /dev/null 2>&1
