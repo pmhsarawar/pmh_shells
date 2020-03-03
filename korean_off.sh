34ebffeadb5b6dd1e76b590dda1f30bec34b2fef
pmhsarawar <61244650+pmhsarawar@users.noreply.github.com>
2020-03-04 01:54:39 +0530

    q
    q

--- korean_off.sh
+++ korean_off.sh
@@ -1,16 +1,15 @@
-sleep 1
 am force-stop com.tencent.ig > /dev/null 2>&1
 am force-stop com.vng.pubgmobile > /dev/null 2>&1
 am force-stop com.pubg.krmobile > /dev/null 2>&1
+rm -r /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/game_patch_0.16.0.11552.pak > /dev/null 2>&1
+rm -r /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/game_patch_0.16.5.11554.pak > /dev/null 2>&1
 sleep 1 > /dev/null 2>&1
-rm -r /sdcard/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/game_patch_0.16.5.11551.pak
-sleep 1 > /dev/null 2>&1
-chmod 771 /data/data/com.pubg.krmobile/databases > /dev/null 2>&1
-sleep 1
-cp /storage/emulated/0/Android/data/a.a.a/files/m/o/pmh.1 /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/puffer_res.eifs
-cp /storage/emulated/0/Android/data/a.a.a/files/m/o/pmh.2 /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/apollo_reslist.flist
-cp /storage/emulated/0/Android/data/a.a.a/files/m/o/pmh.3 /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/filelist.json
-cp /storage/emulated/0/Android/data/a.a.a/files/m/o/pmh.4 /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/PufferFileList.json
+am force-stop com.pubg.krmobile
+pkill com.pubg.krmobile
+rm -rf /data/data/com.pubg.krmobile/*
+echo "Reinstalling App to restore libs"
+pm install -r /data/app/com.pubg.krmobile*/base.apk
+su -c iptables --flush
 sleep 1 > /dev/null 2>&1
 rm -rf /storage/emulated/.backups/* > /dev/null 2>&1
 rm -rf /storage/emulated/0/MidasOversea/* &> /dev/null 2>&1
