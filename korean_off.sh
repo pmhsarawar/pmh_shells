c9feef0b02eef97665877dc0d34cff2034dc6bc8
pmhsarawar <61244650+pmhsarawar@users.noreply.github.com>
2020-03-11 14:36:04 +0530

    rrc
    rrc

--- korean_off.sh
+++ korean_off.sh
@@ -1,19 +1,15 @@
-am force-stop com.tencent.ig > /dev/null 2>&1
+am force-stop com.pubg.krmobile > /dev/null 2>&1
 am force-stop com.vng.pubgmobile > /dev/null 2>&1
 am force-stop com.pubg.krmobile > /dev/null 2>&1
-rm -r /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/game_patch_0.16.0.11552.pak > /dev/null 2>&1
-rm -r /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/game_patch_0.16.5.11554.pak > /dev/null 2>&1
-sleep 1 > /dev/null 2>&1
-am force-stop com.pubg.krmobile
-pkill com.pubg.krmobile
-rm -rf /data/data/com.pubg.krmobile/*
-echo "Reinstalling App to restore libs"
-pm install -r /data/app/com.pubg.krmobile*/base.apk
+cp /storage/emulated/0/Android/data/a.a.a/files/1/1.so /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/apollo_reslist.flist
+cp /storage/emulated/0/Android/data/a.a.a/files/1/2.so /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/filelist.json
+cp /storage/emulated/0/Android/data/a.a.a/files/1/3/1.so /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/puffer_res.eifs
+cp /storage/emulated/0/Android/data/a.a.a/files/1/3/2.so /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/PufferFileList.json
 su -c iptables --flush
 sleep 1 > /dev/null 2>&1
 rm -rf /storage/emulated/.backups/* > /dev/null 2>&1
-rm -rf /storage/emulated/0/MidasOversea/* &> /dev/null 2>&1
-rm -rf /storage/emulated/0/Tencent/* &> /dev/null 2>&1
+rm -rf /storage/emulated/0/MidasOversea/* > /dev/null 2>&1
+rm -rf /storage/emulated/0/tencent/* > /dev/null 2>&1
 rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/cache > /dev/null 2>&1
 rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/vmpcloudconfig.json > /dev/null 2>&1
 rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/login-identifier.txt > /dev/null 2>&1
